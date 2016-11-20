1dce10dd67fdcb37d3602139b86cd584be38763c
Dong Liang <dongliang3571@gmail.com>
2016-11-20 02:08:30 -0500

    added comments

--- qip2_GPU/hw2/fshader_objectMatch_Mac.glsl
+++ qip2_GPU/hw2/fshader_objectMatch_Mac.glsl
@@ -11,7 +11,6 @@ uniform	sampler2D u_Sampler;	// uniform variable for the texture image
 uniform float	  u_StepX_T;
 uniform float	  u_StepY_T;
 uniform	sampler2D u_Sampler_T;
-
 uniform float     u_Sqrt_Sum_T;
 
 
@@ -26,13 +25,17 @@ void main() {
 
     for(int i=-sizeH; i<=sizeH; ++i) {
         for(int j=-sizeW; j<=sizeW; ++j) {
+            // Compute convolution, note template image always has offset in center
             convolve += (texture2D(u_Sampler, vec2(tc.x + j*u_StepX, tc.y + i*u_StepY)) *
                     texture2D(u_Sampler_T, vec2(0.5 + j*u_StepX_T, 0.5 + i*u_StepY_T)));
 
+            // Computer sum of input square
             sum += (texture2D(u_Sampler, vec2(tc.x + j*u_StepX, tc.y + i*u_StepY)) *
                     texture2D(u_Sampler, vec2(tc.x + j*u_StepX, tc.y + i*u_StepY)));
         }
     }
 
+    // first division is to get correlation
+    // second division is to normalize correlation, that is range 0 - 1
 	gl_FragColor = vec4(convolve.rgb/sqrt(sum.rgb)/u_Sqrt_Sum_T, 1.0);
 }
